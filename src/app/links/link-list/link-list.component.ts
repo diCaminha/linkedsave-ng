ce934b8853eb781e076c4e0c129d187350e0cf3d
Denis <dineresc@gmail.com>
2020-11-19 01:16:31 -0300

    cria msg de caso n existir links slvos

--- src/app/links/link-list/link-list.component.ts
+++ src/app/links/link-list/link-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from 'src/app/models/link';
 
 @Component({
   selector: 'ls-link-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./link-list.component.css']
 })
 export class LinkListComponent {
-  title = 'linkedsave-ng';
+
+    links: Link[] = [
+        {id:1, title: 'How Change Angular Material Theme', linkUrl: 'https://examplelink.com'},
+        {id:2, title: 'How Change Angular Material Theme', linkUrl: 'https://examplelink.com'},
+        {id:3, title: 'How Change Angular Material Theme', linkUrl: 'https://examplelink.com'},
+        {id:4, title: 'How Change Angular Material Theme', linkUrl: 'https://examplelink.com'},
+        {id:5, title: 'How Change Angular Material Theme', linkUrl: 'https://examplelink.com'},
+    ];
 }
---
+++ src/app/models/link.ts
@@ -0,0 +1,5 @@
+export interface Link {
+    id: number,
+    title: string,
+    linkUrl: string
+}

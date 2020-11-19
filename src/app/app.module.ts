051a68e58a30a4ca7ca3777500114fc36ab548e2
Denis <dineresc@gmail.com>
2020-11-19 01:06:39 -0300

    criando listagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,12 +13,14 @@ import { AppComponent } from './app.component';
 import { LinkCreateComponent } from './links/link-create/link-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { LinkListComponent } from './links/link-list/link-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LinkCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    LinkListComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
     
   ],
   providers: [],
---
+++ src/app/links/link-list/link-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ls-link-list',
+  templateUrl: './link-list.component.html',
+  styleUrls: ['./link-list.component.css']
+})
+export class LinkListComponent {
+  title = 'linkedsave-ng';
+}

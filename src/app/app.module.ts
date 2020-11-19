eb077eb282399f94872fd5f04cf5fd202d02fcbf
Denis <dineresc@gmail.com>
2020-11-19 00:38:52 -0300

    melhora estilo tela criar link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LinkCreateComponent } from './links/link-create/link-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { LinkCreateComponent } from './links/link-create/link-create.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/links/link-create/link-create.component.ts
+++ src/app/links/link-create/link-create.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+    styleUrls: ['./link-create.component.css'],
     selector: 'ls-link-create',
     templateUrl: './link-create.component.html',
 })

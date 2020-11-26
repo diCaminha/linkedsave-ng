87bebe16ed35237595d8c27d8e23c9bb6e0547ad
Denis <dineresc@gmail.com>
2020-11-26 13:46:32 -0300

    copy link

--- src/app/links/components/card-link/card-link.component.ts
+++ src/app/links/components/card-link/card-link.component.ts
@@ -1,27 +1,34 @@
 import { Component, Input } from '@angular/core';
+import { url } from 'inspector';
 import { Link } from 'src/app/models/link';
 import { LinksService } from '../../links.service';
 
 @Component({
-    selector: 'ls-card-link',
-    templateUrl: './card-link.component.html',
-    styleUrls: ['./card-link.component.css']
+  selector: 'ls-card-link',
+  templateUrl: './card-link.component.html',
+  styleUrls: ['./card-link.component.css'],
 })
 export class CardLinkComponent {
+  @Input()
+  link: Link;
+  @Input()
+  isPreview: boolean = false;
 
-    @Input()
-    link: Link;
-    @Input()
-    isPreview: boolean = false;
+  constructor(private linksService: LinksService) {}
 
-    constructor(private linksService: LinksService) { }
+  onDeleteLink() {
+    this.linksService.deleteLink(this.link.id);
+  }
 
-    onDeleteLink() {
-        this.linksService.deleteLink(this.link.id);
-    }
+  visitPage() {
+    this.linksService.readLink(this.link.id);
+    window.open(this.link.linkUrl, '_blank');
+  }
 
-    visitPage() {
-        this.linksService.readLink(this.link.id);
-        window.open(this.link.linkUrl, "_blank");
-    }
+  copyLinkUrl(linkUrl: HTMLInputElement) {
+    linkUrl.hidden = false;
+    linkUrl.select();
+    document.execCommand('copy');
+    linkUrl.hidden = true;
+  }
 }

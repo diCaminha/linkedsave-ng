31c085eefa507610838352d98c14fd017b3b265d
Denis <dineresc@gmail.com>
2020-12-16 17:31:20 -0300

    add a snackbar service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Route } from '@angular/compiler/src/core';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/shared/snackbar.service';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -21,8 +21,7 @@ export class LoginComponent {
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private _snackBar: MatSnackBar
-
+    private snackbarService: SnackbarService
   ) {}
 
   onLogin() {
@@ -30,20 +29,17 @@ export class LoginComponent {
     const email = this.loginForm.value['email'];
     const password = this.loginForm.value['password'];
 
-    this.authService.login(email, password).subscribe(response => {
+    this.authService.login(email, password).subscribe(
+      (response) => {
         this.authService.savingToken(response);
-        this.openSnackBar();
+        this.snackbarService.openSnackBarSuccess('Welcome to LinkSaved!', null);
         this.router.navigate(['/']);
-    }, err => {
+      },
+      (err) => {
         console.log(err);
+        this.snackbarService.openSnackBarError('Invalid email and/or password.', null);
         this.isLoading = false;
-    })
-  }
-
-  openSnackBar() {
-    let config = new MatSnackBarConfig();
-    config.duration = 3000;
-
-    this._snackBar.open("Welcome to LinkSaved!", null, config);
+      }
+    );
   }
 }
---
+++ src/app/shared/snackbar.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+
+    constructor(private _snackBar: MatSnackBar){}
+
+  openSnackBarSuccess(message, action) {
+    let config = new MatSnackBarConfig();
+    config.duration = 3000;
+
+    this._snackBar.open(message, action, config);
+  }
+
+  openSnackBarError(message, action) {
+    let config = new MatSnackBarConfig();
+    config.panelClass = ['ls-snackbar-error'];
+    config.duration = 3000;
+
+    this._snackBar.open(message, action, config);
+  }
+}
